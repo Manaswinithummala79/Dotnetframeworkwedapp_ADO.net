444062fa05d39bcff52a2b21d3e0ca94dc749e5e
Manaswini Thummala <MThummala_GPS@nec.edu>
2025-05-08 15:30:25 -0400

    crud statements in brand page

--- Dotnetframeworkwedapp_ADO.net/Brand.aspx
+++ Dotnetframeworkwedapp_ADO.net/Brand.aspx
@@ -1,15 +1,68 @@
 ﻿<%@ Page Title="Brand" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Brand.aspx.cs" Inherits="Dotnetframeworkwedapp_ADO.net.Brand" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <%--Added for styling start--%>
+    <style>
+        /*.form-container {
+            margin-top: 30px;
+            background-color: #f9f9f9;
+            padding: 25px;
+            border-radius: 8px;
+            width: 100%;
+            max-width: 600px;
+            box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+        }*/
+        .form-container {
+            margin-top: 30px;
+            padding: 2px;
+            width: 100%;
+            max-width: 600px;
+        }
+
+        .form-container label {
+            font-weight: bold;
+        }
+
+        .form-container input[type="text"] {
+            width: 100%;
+            padding: 8px;
+            margin-top: 4px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        .form-container .asp-button {
+            background-color: #007bff;
+            color: white;
+            border: none;
+            padding: 10px 16px;
+            margin-right: 10px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .form-container .asp-button:hover {
+            background-color: #0056b3;
+        }
+
+        h2 {
+            color: #333;
+        }
+    </style>
+    <%--Added for styling end--%>
+
     <h2>Brand Names.</h2>
-    <asp:FormView ID="FormView1" runat="server" CellPadding="4" ForeColor="Black" BackColor="#CCCCCC" BorderColor="#999999" BorderStyle="Solid" BorderWidth="3px" CellSpacing="2" GridLines="Both">
-        <EditRowStyle BackColor="#000099" Font-Bold="True" ForeColor="White" />
-        <FooterStyle BackColor="#CCCCCC" />
-        <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" />
-        <PagerStyle BackColor="#CCCCCC" ForeColor="Black" HorizontalAlign="Left" />
-        <RowStyle BackColor="White" />
-    </asp:FormView>
-    <asp:GridView ID="BrandsGridView" runat="server" BackColor="#CCCCCC" BorderColor="#999999" BorderStyle="Solid" BorderWidth="3px" CellPadding="4" CellSpacing="2" ForeColor="Black">
+    <asp:GridView ID="BrandsGridView" runat="server" 
+        BackColor="#CCCCCC" 
+        BorderColor="#999999" 
+        BorderStyle="Solid" 
+        BorderWidth="3px" 
+        CellPadding="10" 
+        CellSpacing="10" 
+        ForeColor="Black"
+        Width="70%">
         <FooterStyle BackColor="#CCCCCC" />
         <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" />
         <PagerStyle BackColor="#CCCCCC" ForeColor="Black" HorizontalAlign="Left" />
@@ -20,4 +73,36 @@
         <SortedDescendingCellStyle BackColor="#CAC9C9" />
         <SortedDescendingHeaderStyle BackColor="#383838" />
     </asp:GridView>
+    <div class="form-container">
+            <asp:Table ID="Crudstatementstbl" runat="server" CellSpacing="10" CellPadding="25">
+                <asp:TableRow>
+                    <asp:TableCell>
+                        <asp:Label ID="BrandIDtxtlbl" runat="server" Text="Enter the Brand ID here: " ></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox ID="BrandIDtxt" runat="server"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>
+                        <asp:Label ID="Brandnamelbl" runat="server" Text="Enter the Brand Name here:  "></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell><asp:TextBox ID="BrandNametxt" runat="server"></asp:TextBox></asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>
+                        <asp:Button ID="Updatebtn" runat="server" Text="Update" CssClass="asp-button" onclick="Updatebtn_Click"/>
+                        <asp:Label ID="UpdateBtnResLbl" runat="server"></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Button ID="Deletebtn" runat="server" Text="Delete" CssClass="asp-button" onclick="Deletebtn_Click"/>
+                        <asp:Label ID="Deletereslbl" runat="server"></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Button ID="Insertbtn" runat="server" Text="Insert" CssClass="asp-button" onclick="Insertbtn_Click"/>
+                        <asp:Label ID="Insertreslbl" runat="server"></asp:Label>
+                    </asp:TableCell>
+                    </asp:TableRow>
+            </asp:Table>
+        </div>
 </asp:Content>
--- Dotnetframeworkwedapp_ADO.net/Brand.aspx.cs
+++ Dotnetframeworkwedapp_ADO.net/Brand.aspx.cs
@@ -8,10 +8,120 @@ using System.Data.SqlClient;
 using System.Data;
 
 namespace Dotnetframeworkwedapp_ADO.net
-{
+{           
     public partial class Brand : System.Web.UI.Page
     {
         private string connectionString = @"Server=MANASWINIREDDY\MSSQLSERVERMANU;Database=BikeStores;Trusted_Connection=True;";
+
+        protected void Updatebtn_Click(object sender, EventArgs e)
+        {
+            ResetResultLabels();
+            string brandName = BrandNametxt.Text;
+            int brandID;
+            if (!int.TryParse(BrandIDtxt.Text, out brandID))
+            {
+                UpdateBtnResLbl.Text = "Invalid Brand ID.";
+                return;
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE production.brands SET brand_name = @barndName WHERE brand_id = @brandId";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@barndName", brandName);
+                cmd.Parameters.AddWithValue("@brandId", brandID);
+
+                try
+                {
+                    connection.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                        UpdateBtnResLbl.Text = "Update successful!";
+                    else
+                        UpdateBtnResLbl.Text = "No records updated.";
+                }
+                catch (Exception ex)
+                {
+                    UpdateBtnResLbl.ForeColor = System.Drawing.Color.Red;
+                    UpdateBtnResLbl.Text = "Error: " + ex.Message;
+                }
+            }
+            LoadBrands();
+            ClearInputFields();
+        }
+        protected void Deletebtn_Click(object sender, EventArgs e)
+        {
+            ResetResultLabels();
+            int brandID;
+            if (!int.TryParse(BrandIDtxt.Text, out brandID))
+            {
+                Deletereslbl.Text = "Invalid Brand ID.";
+                return;
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                ResetResultLabels();
+                string query = "Delete FROM production.brands WHERE brand_id = @brandId";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@brandId", brandID);
+
+                try
+                {
+                    connection.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                        Deletereslbl.Text = "Delete successful!";
+                    else
+                        Deletereslbl.Text = "No records deleted.";
+                }
+                catch (Exception ex)
+                {
+                    Deletereslbl.ForeColor = System.Drawing.Color.Red;
+                    Deletereslbl.Text = "Error: " + ex.Message;
+                }
+            }
+            LoadBrands();
+            ClearInputFields();
+        }
+        protected void Insertbtn_Click(object sender, EventArgs e)
+        {
+            ResetResultLabels();
+            string brandName = BrandNametxt.Text;
+            if (!brandName.Equals(""))
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO production.brands (brand_name) VALUES (@barndName)";
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@barndName", brandName);
+
+                    try
+                    {
+                        connection.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                            Insertreslbl.Text = "Insertion Successful!";
+                        else
+                            Insertreslbl.Text = "No records inserted.";
+                    }
+                    catch (Exception ex)
+                    {
+                        Insertreslbl.ForeColor = System.Drawing.Color.Red;
+                        Insertreslbl.Text = "Error: " + ex.Message;
+                    }
+                }
+                LoadBrands();
+            }
+            else
+            {
+                Insertreslbl.Text = "Enter Brand Name to insert the records.";
+            }
+            ClearInputFields();
+        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -33,6 +143,21 @@ namespace Dotnetframeworkwedapp_ADO.net
                 BrandsGridView.DataBind();
             }
         }
+        private void ResetResultLabels()
+        {
+            Insertreslbl.Text = "";
+            UpdateBtnResLbl.Text = "";
+            Deletereslbl.Text = "";
 
+            Insertreslbl.ForeColor = System.Drawing.Color.Black;
+            UpdateBtnResLbl.ForeColor = System.Drawing.Color.Black;
+            Deletereslbl.ForeColor = System.Drawing.Color.Black;
+        }
+
+        private void ClearInputFields()
+        {
+            BrandIDtxt.Text = "";
+            BrandNametxt.Text = "";
+        }
     }
 }
